ec76caa25481a36d50f7916b75208de17ca017ca
Mahmoud-Leghlimi <leghlimia@berea.edu>
2025-02-18 11:22:02 -0500

    commit

--- DDL.sql
+++ DDL.sql
@@ -41,6 +41,32 @@ CREATE TABLE tracks (
     FOREIGN KEY (MediaTypeId) REFERENCES media_types (MediaTypeId),
     FOREIGN KEY (GenreId) REFERENCES genres (GenreId)
 );
+CREATE TABLE customers (
+    customersid integer PRIMARY KEY,
+    lastName NVARCHAR(20),
+    firstName NVARCHAR(20),
+    company NVARCHAR(80),
+    address NVARCHAR(70),
+    city NVARCHAR(40),
+    state NVARCHAR(40),
+    country NVARCHAR(40),
+    postalCode NVARCHAR(10),
+    phone NVARCHAR(24),
+    fax NVARCHAR(24),
+    email NVARCHAR(60),
+    supportRepId integer
+);
+
+CREATE TABLE employees (
+    Employeeid integer PRIMARY KEY,
+    lastName NVARCHAR(20),
+    firstName NVARCHAR(20),
+    title NVARCHAR(30),
+    reportsTo integer,
+    birthDate integer,
+    hireDate integer,
+    address NVARCHAR(70)
+);
 CREATE TABLE invoices (
     InvoiceId INT PRIMARY KEY,
     CustomerId INT,
